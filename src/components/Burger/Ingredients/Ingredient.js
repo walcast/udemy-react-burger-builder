e84f8c3ac52aa5b301d33b4b2cac76bbd08d474b
walter.castillo <walter.castillo@avantica.net>
2020-03-01 15:27:22 -0600

    Include ingredient component

---
+++ src/components/Burger/Ingredients/Ingredient.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classes from './Ingredient.module.css'
+
+const ingredient = (props) => {
+    let burgerIngredient = null;
+
+    switch(props.type) {
+        case('bread-bottom'):
+            burgerIngredient = <div className={classes.BreadBottom}></div>;
+            break;
+        case('bread-top'):
+            burgerIngredient = (
+                <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>
+            );
+            break;
+        case('meat'):
+            burgerIngredient = <div className={classes.Meat}></div>;
+            break;
+        case('cheese'):
+            burgerIngredient = <div className={classes.Cheese}></div>;
+            break;
+        case('salad'):
+            burgerIngredient = <div className={classes.Salad}></div>;
+            break;
+        case('bacon'):
+            burgerIngredient = <div className={classes.Bacon}></div>;
+            break;
+        default:
+            ingredient = null;
+    }
+    return ingredient;
+}
+
+export default ingredient;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
+import classes from './Layout.module.css'
 
 const layout = (props) =>(
     <Fragment>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.content}>
             {props.children}
         </main>
     </Fragment>
